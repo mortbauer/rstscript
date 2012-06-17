df949e9372b392a5e29fb12a8a0436b5ef4beb67
Martin Ortbauer <mortbauer@gmail.com>
2012-06-17 14:13:07 +0300

    completely changed to io based string working, seems quiet good

--- litscript/chunks.py
+++ litscript/chunks.py
@@ -4,33 +4,87 @@ import sys
 import re
 
 
+class LitscriptException(Exception):
+    """Base class for exceptions in this module."""
+    pass
+
+
 def testcase():
     f = open('test.nw', 'rt')
-    c = chunker(f)
-    e = processor(c)
-    post_processor(e)
+    c = read(f)
+    p = process(c)
+    pp = post_process(p)
+    out = open('test.rst', 'wt')
+    write(pp,out)
+    out.close()
     f.close()
 
 
-def post_processor(fileobject:'iterable'):
+def read(fileobject, chunk_start='%<<', chunk_end='%>>', opt_delim='='):
+    """This function produces a generator, which is iterable.
+    The pieces it produces from a readable file have to be delimited
+    by a three character long token at the beginning of a line.
+    It only chenks for the delimiters at the begining of each iterated piece.
+    """
+
+    content = StringIO()
+    delimtercounter = 0
+    re_str = '([a-zA-Z_\-0-9]*)\s*{}\s*([a-zA-Z_\-0-9]*)'
+    get_opts = re.compile(re_str.format(opt_delim))
+    esc_chunk_start = '%{}'.format(chunk_start)
+    esc_chunk_end = '%{}'.format(chunk_end)
+
+    if len(chunk_start) != len(chunk_end):
+        raise ValueError("Length of the start and end tokens must be equal.")
+    else:
+        token_length = len(chunk_start)
+
+    for line in fileobject:
+        line_start = line[:token_length]
+        line_startm = line[:token_length + 1]
+
+        if line_start == chunk_start or line_start == chunk_end:
+            if line_start == chunk_start:
+                pre_args = dict(get_opts.findall(line[token_length:]))
+                yield {'type':'text','content_in':content}
+                content = StringIO()
+                delimtercounter += 1
+            else:
+                post_args = dict(get_opts.findall(line[token_length:]))
+                yield {'type':'code', 'content_in':content,
+                         'pre_args':pre_args, 'post_args':post_args}
+                content = StringIO()
+                delimtercounter -= 1
+        elif line_startm == esc_chunk_start or line_startm == esc_chunk_end:
+            content.write(line[1:])
+        else:
+            content.write(line)
+
+    if delimtercounter != 0:
+        raise LitscriptException('You may have forgotten to end a code chunk.')
+
+    yield {'type':'text','content_in':content}
+
+
+def pre_process(fileobject):
     for chunk in fileobject:
-        print(chunk,chunk['content'].getvalue())
+        yield chunk
 
 
-def processor(fileobject:'iterable'):
+def process(fileobject):
     """Executes the given expression in the given local namespace."""
     #:store the reference to the std's
     stdout = sys.stdout
     stderr = sys.stderr
-    #:instantiate StringIO object
-    stdout_ = StringIO()
-    stderr_ = StringIO()
     namesp_global = {}
     namesp_local = {}
 
     for chunk in fileobject:
+        #:instantiate StringIO object
+        stdout_ = StringIO()
+        stderr_ = StringIO()
         if chunk['type'] == 'code':
-            code = chunk['content'].getvalue()
+            code = chunk['content_in'].getvalue()
             #print('CODE: ',code,type(code),repr(code))
             try:
                 #:change std's
@@ -45,40 +99,21 @@ def processor(fileobject:'iterable'):
                 sys.stdout = stdout
                 sys.stderr = stderr
 
-            chunk['stdout'] = stdout_.getvalue()
-            chunk['stderr'] = stderr_.getvalue()
+            chunk['stdout'] = stdout_
+            chunk['stderr'] = stderr_
         yield chunk
 
 
-def chunker(fileobject:'iterable', chunk_start='%<<', chunk_end='%>>'):
-    """This function produces a generator, which is iterable.
-    The pieces it produces from a readable file have to be delimited
-    by a three character long token at the beginning of a line.
-    It only chenks for the delimiters at the begining of each iterated piece.
-    """
-    content = StringIO()
-    get_opts = re.compile('([a-zA-Z_\-0-9]*)\s*=\s*([a-zA-Z_\-0-9]*)')
-
-    if len(chunk_start) != len(chunk_end):
-        raise ValueError("The length of the start and end"
-                         "tokens must be equal.")
-    else:
-        token_length = len(chunk_start)
-
-    for line in fileobject:
-        line_start = line[:token_length]
-
-        if line_start == chunk_start or line_start == chunk_end:
-            if line_start == chunk_start:
-                pre_args = dict(get_opts.findall(line[token_length:]))
-                yield {'type':'text','content':content}
-                content = StringIO()
-            else:
-                post_args = dict(get_opts.findall(line[token_length:]))
-                yield {'type':'code', 'content':content,
-                         'pre_args':pre_args, 'post_args':post_args}
-                content = StringIO()
+def post_process(fileobject):
+    for chunk in fileobject:
+        if chunk['type'] == 'text':
+            chunk['content_out'] = (chunk['content_in'],)
         else:
-            content.write(line)
+            chunk['content_out'] = (chunk['content_in'],chunk['stdout'])
+        yield chunk
 
-    yield {'type':'text','content':content}
+
+def write(fileobject_in, fileobject_out):
+    for chunk in fileobject_in:
+        for stream in chunk['content_out']:
+            fileobject_out.write(stream.getvalue())
